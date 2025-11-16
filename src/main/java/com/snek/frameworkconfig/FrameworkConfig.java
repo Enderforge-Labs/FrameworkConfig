e3b5dada35092b9278c14e62d1cae56db66dbf6f
UwU_Snek <xiaoju345@gmail.com>
2025-11-16 17:48:53 +0100

    Define a custom initialization phase Add an accessible library ID constant

--- src/main/java/com/snek/frameworkconfig/FrameworkConfig.java
+++ src/main/java/com/snek/frameworkconfig/FrameworkConfig.java
@@ -1,14 +1,25 @@
 package com.snek.frameworkconfig;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.minecraft.resources.ResourceLocation;
 
 
 
 
 public class FrameworkConfig implements ModInitializer {
+    public static final String LIB_ID = "frameworkconfig";
+    public static final ResourceLocation INIT_PHASE_ID = new ResourceLocation(LIB_ID, "init");
+
 
     @Override
     public void onInitialize() {
         System.out.println("FrameworkConfig loaded :3");
+
+
+        // Register initialization
+        ServerLifecycleEvents.SERVER_STARTING.register(INIT_PHASE_ID, server -> {
+            // Empty
+        });
     }
 }
